d8fc802d452cd1a5ae964d45dc40ea79a07b2fc2
Sergey Simashov <simashov@gmail.com>
2023-08-01 13:15:58 +0300

    Initial Commit

---
+++ SurfCVApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfCVApp
+//
+//  Created by Sergey Simashov on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
